fad81824cbc4c7973e74ae0dee5896535dd0f98d
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-12 22:13:02 +0100

    feat: color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,34 +1,23 @@
-// Pobranie przycisków
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
-// Dodanie obsługi zdarzeń dla przycisków
 startButton.addEventListener('click', startColorChanging);
 stopButton.addEventListener('click', stopColorChanging);
 
-// Funkcja generująca losowy kolor
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let intervalId; // Zmienna przechowująca identyfikator interwału
+let intervalId;
 
-// Funkcja uruchamiająca zmianę koloru co sekundę
 function startColorChanging() {
-  // Dezaktywacja przycisku "start"
   startButton.disabled = true;
-
-  // Uruchomienie interwału zmiany koloru co sekundę
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
-// Funkcja zatrzymująca zmianę koloru
 function stopColorChanging() {
-  // Aktywacja przycisku "start"
   startButton.disabled = false;
-
-  // Zatrzymanie interwału
   clearInterval(intervalId);
 }
