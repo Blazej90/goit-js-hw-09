0ce9c9872a35765e67ecb13145e51ee888836e17
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-12 21:58:38 +0100

    feat: promise.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+// Obsługa formularza
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const firstDelay = parseInt(formData.get('delay'), 10);
+    const step = parseInt(formData.get('step'), 10);
+    const amount = parseInt(formData.get('amount'), 10);
+
+    for (let i = 0; i < amount; i++) {
+      const position = i + 1;
+      const currentDelay = firstDelay + i * step;
+
+      try {
+        const result = await createPromise(position, currentDelay);
+        console.log(
+          `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
+        );
+      } catch (error) {
+        console.log(
+          `❌ Rejected promise ${error.position} in ${error.delay}ms`
+        );
+      }
+    }
+  });
+});
